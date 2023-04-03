e98dbc8a7bb805456ef5c27e994a08ed69787116
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-04-03 18:40:52 +0900

    6ixSense frontend 초기 세팅

---
+++ src/Router.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
+import Signin from './pages/Signin/Signin';
+import Signup from './pages/Signup/Signup';
+import ProductList from './pages/ProductList/ProductList';
+import ProductDetail from './pages/ProductDetail/ProductDetail';
+import Gift from './pages/Gift/Gift';
+import Main from './pages/Main/Main';
+import Payment from './pages/Payment/Payment';
+import Cart from './pages/Cart/Cart';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/ProductList" element={<ProductList />} />
+        <Route path="/ProductDetail" element={<ProductDetail />} />
+        <Route path="/Payment" element={<Payment />} />
+        <Route path="/Gift" element={<Gift />} />
+        <Route path="/Cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
+
+export default Router;
---
+++ src/pages/Main/Main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Main = () => {
+  return <div>아녕하세요</div>;
+};
+
+export default Main;
