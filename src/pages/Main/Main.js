a3563b8671f41c6b42b671040d5445a6652487cd
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-04-05 12:09:15 +0900

    ADD : 메인 페이지 중간까지 구현

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,36 +1,123 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IoIosArrowBack } from 'react-icons/io';
 import { IoIosArrowForward } from 'react-icons/io';
+import todayBg from '../assets/images/today_product_1.jpeg';
 import './Main.scss';
 
 const Main = () => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/data.json', {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(data => setProductList(data));
+  }, []);
+
   return (
     <div className="mainBody">
       <div className="bannerBody">
-        {BANNER_INFO.map(function (a) {
+        {BANNER_INFO.map(function (banner) {
           return (
-            <>
-              <div
-                key={a.id}
-                className="mainBanner"
-                style={{ backgroundImage: `url(${a.bannerImg})` }}
-              />
-              {/* <div className="alignText">
-                <span className="bannerTitle">{a.bannerTitle}</span>
-                <span className="bannerText">{a.bannerText}</span>
-              </div> */}
-            </>
+            <div
+              key={banner.id}
+              className="mainBanner"
+              style={{ backgroundImage: `url(${banner.bannerImg})` }}
+            >
+              <div className="alignText">
+                <span className="bannerTitle">{banner.bannerTitle}</span>
+                <span className="bannerText">{banner.bannerText}</span>
+              </div>
+            </div>
           );
         })}
       </div>
       <div className="alignBox">
-        <IoIosArrowBack className="arrowIcon" />
-        <IoIosArrowForward className="arrowIcon" />
+        <IoIosArrowBack className="mainArrowIcon" />
+        <IoIosArrowForward className="mainArrowIcon" />
       </div>
-      <div className="mainPagination"></div>
+      {/* <div className="mainPagination"></div> */}
       <div className="mainContents">
-        <div className="recommendTitle">오늘의 향을 추천해 드려요</div>
-        <div className="recommendList" />
+        <div className="mainNotice">
+          <p className="notice">공지사항</p>
+          <div className="noticeBox">
+            <ul className="noticeList">
+              <li>2023년 3월 다다일상 챌린지 당첨자 발표</li>
+            </ul>
+            <IoIosArrowForward style={{ marginRight: '10px' }} />
+          </div>
+        </div>
+        <div className="recommendSector">
+          <div className="recommendTitle">오래 기억될 순간들</div>
+          <div className="recommendList">
+            {productList.map(function (product) {
+              return (
+                <div className="recommendProduct" key={product.id}>
+                  <div className="productInfo">
+                    <img
+                      className="productImg"
+                      src={product.pdImage}
+                      alt="상품 이미지"
+                    />
+                    <p className="productName">
+                      <a>{product.pdName}</a>
+                    </p>
+                    <p className="pdPrice">{product.pdPrice}</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {/* <div className="miniAlignBox">
+          <IoIosArrowBack className="miniArrowIcon" />
+          <IoIosArrowForward className="miniArrowIcon" />
+        </div> */}
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '60px',
+            }}
+          >
+            <button
+              type="button"
+              style={{
+                padding: '15px 60px',
+                border: '1px solid lightgray',
+                borderRadius: '5px',
+                textAlign: 'center',
+                display: 'flex',
+                alignItems: 'center',
+                color: '#252525',
+                backgroundColor: 'white',
+              }}
+            >
+              더 보기 <IoIosArrowForward />
+            </button>
+          </div>
+        </div>
+        <div className="todayRecommend">
+          <div
+            className="todayBg"
+            style={{ backgroundImage: `url(${todayBg})` }}
+          />
+          <div className="todayTextArea">
+            <div className="todayTimeText">
+              <h3 style={{ fontSize: '26px', fontWeight: '500' }}>
+                오늘의 기억
+              </h3>
+              <p className="timeText">01:54:55</p>
+            </div>
+            <h4 style={{ fontSize: '28px', fontWeight: '500' }}>
+              베스트 인센스 컬렉션
+            </h4>
+            <div className="todayPriceText">
+              <div className="priceText">24000원</div>
+              <div className="discountText">21600원</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
@@ -39,35 +126,30 @@ const Main = () => {
 const BANNER_INFO = [
   {
     id: 1,
-    bannerColor: '#8D7B68',
     bannerImg: 'images/osulloc_1.png',
     bannerTitle: '첫 번째 배너 타이틀',
     bannerText: '첫 번째의 소개글',
   },
   {
     id: 2,
-    bannerColor: '#A4907C',
     bannerImg: 'images/osulloc_2.jpeg',
     bannerTitle: '두 번째 배너 타이틀',
     bannerText: '두 번째의 소개글',
   },
   {
     id: 3,
-    bannerColor: '#C8B6A6',
     bannerImg: 'images/osulloc_3.jpeg',
     bannerTitle: '세 번째 배너 타이틀',
     bannerText: '세 번째의 소개글',
   },
   {
     id: 4,
-    bannerColor: '#F1DEC9',
     bannerImg: 'images/osulloc_4.jpeg',
     bannerTitle: '네 번째 배너 타이틀',
     bannerText: '네 번째의 소개글',
   },
   {
     id: 5,
-    bannerColor: '#F5EBEB',
     bannerImg: 'images/osulloc_5.jpeg',
     bannerTitle: '다섯 번째 배너 타이틀',
     bannerText: '다섯 번째의 소개글',
