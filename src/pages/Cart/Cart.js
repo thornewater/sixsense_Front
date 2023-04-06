19b059dabf7ffb39f300f1558cb641a73a148655
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-04-06 14:48:42 +0900

    ADD : 장바구니 레이아웃 구현 중

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,8 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Cart.scss';
 
 const Cart = () => {
-  return <div>장바구니 페이지 입니다.</div>;
+  const [lists, setLists] = useState([]);
+  useEffect(() => {
+    fetch('/public/data/cart.json', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    })
+      .then(res => res.json())
+      .then(data => setLists(data));
+  }, []);
+
+  return (
+    <div className="Cart">
+      <section className="cartBanner">
+        <div className="cartTitle">
+          <h2>장바구니</h2>
+        </div>
+      </section>
+      <section className="cartInner">
+        <div className="cartChoice">
+          <div className="allSelect">전체선택</div>
+          <div className="selectDel">선택 삭제</div>
+        </div>
+        <div className="cartList" />
+      </section>
+    </div>
+  );
 };
 
 export default Cart;
